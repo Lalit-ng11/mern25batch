6cba1ab0c4199797fe2bfc99bd4ea272100ff6d8
Lalit-ng11 <lalitpatil.itp@gmail.com>
2025-07-09 10:48:04 +0530

    this is mern proj.

---
+++ BE/models/User.js
@@ -0,0 +1,24 @@
+// import { required } from "joi";
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        default:Date.now()
+    }
+})
+// module.exports = mongoose.model("userExpense",UserSchema);
+export default  mongoose.model("userExpense",UserSchema);
